67ce01da779c46fd045519dc7dd7fbecc79945c3
Jules Dourlens <jdourlens@localhost.localdomain>
2021-02-18 10:25:48 +0100

    clean

--- schemas/tables/meta_events.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS meta_events (
-    topic TEXT,
-    name TEXT,
-    parameters JSON,
-    CONSTRAINT unique_meta_event UNIQUE(topic)
-);
-
-CREATE INDEX meta_events_topic ON meta_events USING btree (topic);
--- schemas/views/000_remove_old.sql
+++
@@ -1,4 +0,0 @@
-DELETE from blocks WHERE timestamp < CURRENT_DATE - INTERVAL '90 day';
-DELETE from events WHERE timestamp < CURRENT_DATE - INTERVAL '90 day';
-DELETE from transactions WHERE timestamp < CURRENT_DATE - INTERVAL '90 day';
-DELETE from token_transfers WHERE timestamp < CURRENT_DATE - INTERVAL '90 day';
