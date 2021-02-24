0c0b5d6264189e3a2c3d722d402cab21a4923a75
Jules Dourlens <jdourlens@localhost.localdomain>
2021-02-24 11:32:23 +0100

    Added nfts route to api

--- api/app.js
+++ api/app.js
@@ -22,7 +22,7 @@ app.use(ddos.express);
 app.use(cors());
 
 app.get('/activity', async function (req, res) {
-  let currentPage = req.params.page != null ? parseInt(req.params.page) : 0;
+  let currentPage = req.query.page != null ? parseInt(req.query.page) : 0;
   let query = storage.knex
     .select("*")
     .from("nft20_history")
@@ -36,10 +36,31 @@ app.get('/activity', async function (req, res) {
 })
 
 app.get('/pools', async function (req, res) {
-  let currentPage = req.params.page != null ? parseInt(req.params.page) : 0;
+  let currentPage = req.query.page != null ? parseInt(req.query.page) : 0;
   let query = storage.knex
     .select("*")
     .from("nft20_pool_view")
+  let result = await
+    query.paginate({
+      perPage: req.query.perPage ? parseInt(req.query.perPage) : 50,
+      currentPage: currentPage != null,
+      isLengthAware: true,
+    });
+  res.status(200).json(result);
+})
+
+app.get('/nfts', async function (req, res) {
+  let currentPage = req.query.page != null ? parseInt(req.query.page) : 0;
+  let query = null
+  if (req.query.pool != null) {
+    query = storage.knex
+    .select("*")
+    .from("nft20_nfts_view").where("pool", req.query.pool.toLowerCase())
+  } else {
+    query = storage.knex
+    .select("*")
+    .from("nft20_nfts_view")
+  }
   let result = await
     query.paginate({
       perPage: req.query.perPage ? parseInt(req.query.perPage) : 50,
