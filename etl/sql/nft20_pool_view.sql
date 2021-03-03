ca404472253835e6f5468d8f7fdb49e945f2b7e0
Jules Dourlens <jdourlens@gmail.com>
2021-03-03 14:46:04 +0100

    Improved pool view

--- etl/sql/nft20_pool_view.sql
+++ etl/sql/nft20_pool_view.sql
@@ -9,13 +9,13 @@ AS
     p.lp_usd_balance,
     p.nft_usd_price,
     p.nft_eth_price,
-    sum(h.amount) AS nft_locked,
-    sum(h.amount) * 100::numeric AS token_supply,
-    sum(h.total_transfers) AS total_nft_transfers,
-    count(DISTINCT h."user") AS pool_users,
-        p.logo_url 
+    coalesce(sum(h.amount), 0) AS nft_locked,
+    coalesce(sum(h.amount) * 100::numeric, 0) AS token_supply,
+    coalesce(sum(h.total_transfers), 0) AS total_nft_transfers,
+    coalesce(count(DISTINCT h."user"), 0) AS pool_users,
+    p.logo_url
    FROM nft20_pair p
      LEFT JOIN nft20_history h ON h.address::text = p.address::text
-     where p.hidden = false
+     where p.hidden != true 
   GROUP BY p.address
   ORDER BY p.name;
