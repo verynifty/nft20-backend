054685b53bad6c08f31ac63e887ec561f20aa67e
Adam <surfcoderepeat@protonmail.com>
2021-03-03 22:50:45 -0600

    query activity by user

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 var express = require("express");
-require('dotenv').config()
+require("dotenv").config();
 
 storage = new (require("../etl/utils/storage"))({
   user: process.env.NFT20_DB_USER,
@@ -15,58 +15,53 @@ var cors = require("cors");
 var app = express();
 app.use(cors());
 
-app.get('/activity', async function (req, res) {
+app.get("/activity", async function (req, res) {
   let currentPage = req.query.page != null ? parseInt(req.query.page) : 0;
   let query = storage.knex
     .select("*")
     .from("nft20_history")
-  let result = await
-    query.paginate({
-      perPage: req.query.perPage ? parseInt(req.query.perPage) : 50,
-      currentPage: currentPage ? currentPage : 0,
-      isLengthAware: true,
-    });
-  res.setHeader('Cache-Control', 's-max-age=60, stale-while-revalidate')
+    .where(user, req.query.user ? req.query.user : "*");
+  let result = await query.paginate({
+    perPage: req.query.perPage ? parseInt(req.query.perPage) : 50,
+    currentPage: currentPage ? currentPage : 0,
+    isLengthAware: true,
+  });
+  res.setHeader("Cache-Control", "s-max-age=60, stale-while-revalidate");
   res.status(200).json(result);
-})
+});
 
-app.get('/pools', async function (req, res) {
+app.get("/pools", async function (req, res) {
   let currentPage = req.query.page != null ? parseInt(req.query.page) : 0;
-  let query = storage.knex
-    .select("*")
-    .from("nft20_pool_view")
-  let result = await
-    query.paginate({
-      perPage: req.query.perPage ? parseInt(req.query.perPage) : 50,
-      currentPage: currentPage ? currentPage : 0,
-      isLengthAware: true,
-    });
-  res.setHeader('Cache-Control', 's-max-age=60, stale-while-revalidate')
+  let query = storage.knex.select("*").from("nft20_pool_view");
+  let result = await query.paginate({
+    perPage: req.query.perPage ? parseInt(req.query.perPage) : 50,
+    currentPage: currentPage ? currentPage : 0,
+    isLengthAware: true,
+  });
+  res.setHeader("Cache-Control", "s-max-age=60, stale-while-revalidate");
   res.status(200).json(result);
-})
+});
 
-app.get('/nfts', async function (req, res) {
+app.get("/nfts", async function (req, res) {
   let currentPage = req.query.page != null ? parseInt(req.query.page) : 0;
-  let query = null
+  let query = null;
   if (req.query.pool != null) {
     query = storage.knex
       .select("*")
-      .from("nft20_nfts_view").where("pool", req.query.pool.toLowerCase())
-  } else {
-    query = storage.knex
-      .select("*")
       .from("nft20_nfts_view")
+      .where("pool", req.query.pool.toLowerCase());
+  } else {
+    query = storage.knex.select("*").from("nft20_nfts_view");
   }
-  let result = await
-    query.paginate({
-      perPage: req.query.perPage ? parseInt(req.query.perPage) : 50,
-      currentPage: currentPage ? currentPage : 0,
-      isLengthAware: true,
-    });
-  res.setHeader('Cache-Control', 's-max-age=60, stale-while-revalidate')
+  let result = await query.paginate({
+    perPage: req.query.perPage ? parseInt(req.query.perPage) : 50,
+    currentPage: currentPage ? currentPage : 0,
+    isLengthAware: true,
+  });
+  res.setHeader("Cache-Control", "s-max-age=60, stale-while-revalidate");
   res.status(200).json(result);
-})
+});
 
 //app.listen(7878)
 
-module.exports = app
+module.exports = app;
