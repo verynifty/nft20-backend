30c7474cc619a6cfeaa7b0f438de7ded1935a382
Jules Dourlens <jdourlens@gmail.com>
2021-03-05 14:48:05 +0100

    Added Tranding pools

---
+++ etl/sql/nft_hot_pool_view.sql
@@ -0,0 +1,8 @@
+CREATE OR REPLACE view public.nft_hot_pool_view AS
+SELECT "public"."nft20_pair".*, count(distinct "Nft20 History"."user") AS "count"
+FROM "public"."nft20_pair"
+LEFT JOIN "public"."nft20_history" "Nft20 History" ON "public"."nft20_pair"."address" = "Nft20 History"."address"
+WHERE CAST("Nft20 History"."timestamp" AS date) BETWEEN CAST((CAST(now() AS timestamp) + (INTERVAL '-7 day')) AS date)
+   AND CAST(now() AS date)
+GROUP BY "public"."nft20_pair"."address"
+ORDER BY "count" DESC
