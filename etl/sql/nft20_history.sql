69247a55bbe1e33ed1b0ae31d806336937c5e510
Jules Dourlens <jdourlens@gmail.com>
2021-03-16 12:29:22 +0100

    Added volume to history items

--- etl/sql/nft20_history.sql
+++ etl/sql/nft20_history.sql
@@ -17,6 +17,8 @@ AS SELECT p.address,
     a."user",
     array_agg(a.id) AS ids,
     array_agg(a.amount) AS amounts,
+    array_agg(n.nft_title) AS nft_name,
+    array_agg(n.nft_image) AS nft_image,
     sum(abs(a.amount)) AS total_transfers,
     sum(a.amount) AS amount,
         CASE
@@ -24,9 +26,18 @@ AS SELECT p.address,
             WHEN sum(a.amount) < 0::numeric THEN 'Withdraw'::text
             WHEN sum(a.amount) = 0::numeric THEN 'Swap'::text
             ELSE NULL::text
-        END AS type
+        END AS type,
+            CASE
+            WHEN sum(a.amount) = 0::numeric THEN    sum(abs(a.amount) /2 * p.nft_eth_price )
+            ELSE  sum(abs(a.amount)  * p.nft_eth_price )
+        END AS volume_eth,
+         CASE
+            WHEN sum(a.amount) = 0::numeric THEN    sum(abs(a.amount) /2 * p.nft_usd_price )
+            ELSE  sum(abs(a.amount)  * p.nft_usd_price )
+        END AS volume_usd
    FROM nft20_action a,
-    nft20_pair p
-  WHERE a.pool::text = p.address::text
+    nft20_pair p,
+    nft20_nft n
+  WHERE a.pool::text = p.address::text AND n.nft_contract::text = p.nft::text AND n.nft_id = a.id
   GROUP BY a.blocknumber, a.transactionhash, a."from", a."timestamp", a."to", a.pool, p.nft, a."user", p.address, p.nft_type, p.lp_eth_balance, p.lp_usd_balance, p.nft_eth_price, p.nft_usd_price, p.name, p.symbol
   ORDER BY a.blocknumber DESC;
