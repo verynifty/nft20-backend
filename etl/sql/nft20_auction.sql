a85b5c5cfd2312878d070c318855ccdb1c0f425e
Jules Dourlens <jdourlens@gmail.com>
2021-03-30 11:52:49 +0200

    Add auction table

---
+++ etl/sql/nft20_auction.sql
@@ -0,0 +1,14 @@
+CREATE TABLE public.nft20_auctions
+(
+    auction_id numeric NULL,
+    "seller" varchar NULL,
+    "pair" varchar NULL,
+    tokenId numeric NULL,
+    "starting_price" numeric NULL,
+    "ending_price" numeric NULL,
+    "starting_time" timestamp NULL,
+    "ending_time" timestamp NULL,
+    "duration" numeric NULL,
+    "ended" boolean null,
+    CONSTRAINT unique_auction_id UNIQUE (auction_id)
+);
