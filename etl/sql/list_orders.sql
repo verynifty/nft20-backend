40df0ca5437319c41c3c903d4168c9bc2f337e9e
Jules Dourlens <jdourlens@gmail.com>
2021-04-08 13:19:55 +0200

    Add collection information to query

--- etl/sql/list_orders.sql
+++ etl/sql/list_orders.sql
@@ -32,9 +32,9 @@ create or REPLACE view listing_view as
 select listing.title, listing .description , listing .author, listing ."timestamp", listing .cancelled, listing .nonce, listing .sold, listing .token_price , array_agg(distinct (a.nft_contract)) as contracts, SUM(a.nft_amount) as nft_quantity, count(a) as nft_distincts, CASE WHEN count(a) = 0 THEN ARRAY[]::json[] ELSE array_agg(a.nfts) END AS nfts, listing .id
 from list_listing listing LEFT OUTER JOIN
     (
-    select elem.listing_id , elem.nft_amount, nft.nft_contract , json_build_object('id', nft.nft_id, 'contract', nft.nft_contract , 'amount', elem .nft_amount, 'title', nft.nft_title , 'image', nft.nft_image ) as nfts
-    FROM list_listing_elem elem, nft20_nft nft
-    where elem.nft_contract = nft.nft_contract and elem .nft_id = nft.nft_id 
+    select elem.listing_id , elem.nft_amount, nft.nft_contract , json_build_object('id', nft.nft_id, 'contract', nft.nft_contract , 'amount', elem .nft_amount, 'title', nft.nft_title , 'image', nft.nft_image, 'collection_name', nc.collection_name , 'collection_description', nc.collection_description , 'collection_image', nc.image_url, 'collection_type', nc.collection_type ) as nfts
+    FROM list_listing_elem elem, nft20_nft nft, nft20_collection nc
+    where elem.nft_contract = nft.nft_contract and elem .nft_id = nft.nft_id and nft.nft_contract = nc.contract_address 
   ) a on  listing.id = a.listing_id
 group by listing.title, listing .description , listing .author, listing ."timestamp", listing .cancelled, listing .nonce, listing .sold, listing .token_price, listing .id
 order by listing."timestamp" DESC
