78d0325741c56ffea9e81b8a4c967bec5a16c618
grandsmarquis <jdourlens@gmail.com>
2021-05-01 11:40:17 +0200

    Add NFT api

--- api/nft.js
+++ api/nft.js
@@ -22,46 +22,44 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
-function getChainID(net = null) {
-    let network = net != null ? parseInt(net) : 0;
-    if (network == 0) {
-        return 1;
-    }
-    else if (network == 1) {
-        return 80001;
-    }
-}
 
-app.get("/contract/:contract/:user/", async function (req, res) {
-    let network = getChainID(req.query.network)
-    let result = await Axios.get("https://api.covalenthq.com/v1/" + network + "/address/" + req.params.user + "/balances_v2/?nft=true",
-    {
-        auth: {
-            user: process.env.COVALENT_KEY
-        }
-    })
-    let r = []
-    console.log(result.data.data.items)
-    for (const it of result.data.data.items) {
-        if (it.contract_address.toLowerCase() == req.params.contract.toLowerCase()) {
-            for (const n of it.nft_data) {
-                console.log(n)
-                let nft = {
-                    nft_contract:  req.params.contract.toLowerCase(),
-                    nft_id: n.token_id,
-                    nft_title: n.external_data.name,
-                    nft_description: n.external_data.description,
-                    nft_image: n.external_data.image,
-                    nft_original_image: n.external_data.image,
-                    nft_trait: JSON.stringify(n.external_data.attributes)
-                }
-                console.log(nft)
-                r.push(nft)
+async function getMNFTFromUser(address) {
+    let res = await Axios.post('https://api.thegraph.com/subgraphs/name/grandsmarquis/erc1155matic', {
+        query: `
+        {
+            balances (where: {account: "` + address.toLowerCase() + `"}) {
+              id
+            value
+            token {
+              identifier
+              registry {
+                id
+              }
+            }
             }
         }
-    }
-    console.log()
+        
+        `
+      })
+      let result = []
+      let balances = res.data.data.balances
+      for (const balance of balances) {
+          result.push({
+            contract_address: balance.token.registry.id,
+            nft_id: balance.token.identifier,
+            amount: balance.value
+          })
+      }
+      return (result)
+}
 
+app.get("/matic/user/:user/", async function (req, res) {
+  let NFTs = await getMNFTFromUser(req.params.user);
+  if (req.query.contract_address != null) {
+      NFTs = NFTs.filter(NFT => NFT.contract_address == req.query.contract_address.toLowerCase())
+  }
+  res.setHeader("Cache-Control", "s-max-age=60, stale-while-revalidate");
+  res.status(200).json(NFTs);
 })
 
 
