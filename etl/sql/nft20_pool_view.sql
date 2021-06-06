99d0b7682324b71bc316e3ec07465e5a30108f98
grandsmarquis <jdourlens@gmail.com>
2021-06-06 15:31:06 +0200

    Updated pool view

--- etl/sql/nft20_pool_view.sql
+++ etl/sql/nft20_pool_view.sql
@@ -1,4 +1,5 @@
-CREATE OR REPLACE VIEW  public.nft20_pool_view AS 
+CREATE OR REPLACE VIEW public.nft20_pool_view
+as
 SELECT p.address,
     p.nft,
     p.nft_type,
@@ -9,22 +10,40 @@ SELECT p.address,
     p.nft_usd_price,
     p.nft_eth_price,
     p.network,
+    c.banner_url ,
+    c.image_url,
+    c.collection_description,
+    c.collection_total_assets,
+    c.external_url ,
+    c.featured_image_url ,
+    c.telegram_url ,
+    c.twitter_username ,
+    c.number_of_owners ,
     COALESCE(sum(h.amount), 0::numeric) AS nft_locked,
     COALESCE(sum(h.amount) * 100::numeric, 0::numeric) AS token_supply,
     COALESCE(sum(h.total_transfers), 0::numeric) AS total_nft_transfers,
     COALESCE(count(DISTINCT h."user"), 0::bigint) AS pool_users,
     p.logo_url,
     COALESCE(count(DISTINCT h."user") FILTER (WHERE h."timestamp" > (CURRENT_DATE - '1 day'::interval)), 0::bigint) AS users_today,
-    COALESCE(count(DISTINCT h."user") FILTER (WHERE h."timestamp" > (CURRENT_DATE - '7 day'::interval)), 0::bigint) AS users_weekly,
+    COALESCE(count(DISTINCT h."user") FILTER (WHERE h."timestamp" > (CURRENT_DATE - '7 days'::interval)), 0::bigint) AS users_weekly,
     p.nft_value,
-     COALESCE(sum(h.volume_usd)  FILTER (WHERE h."timestamp" > (CURRENT_DATE - '1 day'::interval)), 0::bigint) AS volume_today_usd,
-    COALESCE(sum(h.volume_usd) FILTER (WHERE h."timestamp" > (CURRENT_DATE - '7 day'::interval)), 0::bigint) AS volume_weekly_usd,
-    COALESCE(sum(h.volume_eth)  FILTER (WHERE h."timestamp" > (CURRENT_DATE - '1 day'::interval)), 0::bigint) AS volume_today_eth,
-    COALESCE(sum(h.volume_eth) FILTER (WHERE h."timestamp" > (CURRENT_DATE - '7 day'::interval)), 0::bigint) AS volume_weekly_eth,
-    COALESCE(sum(h.volume_usd), 0)  as volume_usd,
-        COALESCE(sum(h.volume_eth), 0)  as volume_eth
+    COALESCE(sum(h.volume_usd) FILTER (WHERE h."timestamp" > (CURRENT_DATE - '1 day'::interval)), 0::bigint::numeric) AS volume_today_usd,
+    COALESCE(sum(h.volume_usd) FILTER (WHERE h."timestamp" > (CURRENT_DATE - '7 days'::interval)), 0::bigint::numeric) AS volume_weekly_usd,
+    COALESCE(sum(h.volume_eth) FILTER (WHERE h."timestamp" > (CURRENT_DATE - '1 day'::interval)), 0::bigint::numeric) AS volume_today_eth,
+    COALESCE(sum(h.volume_eth) FILTER (WHERE h."timestamp" > (CURRENT_DATE - '7 days'::interval)), 0::bigint::numeric) AS volume_weekly_eth,
+    COALESCE(sum(h.volume_usd), 0::numeric) AS volume_usd,
+    COALESCE(sum(h.volume_eth), 0::numeric) AS volume_eth
    FROM nft20_pair p
      LEFT JOIN nft20_history h ON h.address::text = p.address::text
+      LEFT JOIN nft20_collection c ON c.contract_address::text = p.nft::text
   WHERE p.hidden = false
-  GROUP BY p.address
+  GROUP BY p.address,    c.banner_url ,
+    c.image_url,
+    c.collection_description,
+    c.collection_total_assets,
+    c.external_url ,
+    c.featured_image_url ,
+    c.telegram_url ,
+    c.twitter_username ,
+    c.number_of_owners 
   ORDER BY p.name;
