05f96b63df74d6c7a141c229b38cc9424e6562a0
grandsmarquis <jdourlens@gmail.com>
2021-06-18 18:27:36 +0200

    split

--- etl/utils/nft20.js
+++ etl/utils/nft20.js
@@ -408,8 +408,9 @@ NFT20.prototype.getData = async function (
   let pairs = await this.getPairs(true);
   let i = 0;
   for (const pair of pairs) {
+    
     console.log("Get events for pair", pair.name, blocknumber, i++, "/" + pairs.length, "@", pair.address);
-    if (pair.address == "0x1b7c23efc2198c5ffa907b7ac0891216d6c9baa2") {
+    if (pair.address == "0x1b7c23efc2198c5ffa907b7ac0891216d6c9baa2" || i < 44) {
       continue;
     }
     const TwentyContract = new this.ethereum.w3.eth.Contract(
