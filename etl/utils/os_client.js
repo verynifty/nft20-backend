b7b5c0b84e56e7a41ffb1b03083682ad322f18a9
grandsmarquis <jdourlens@gmail.com>
2021-06-21 14:26:41 +0200

    add collec

--- etl/utils/os_client.js
+++ etl/utils/os_client.js
@@ -10,10 +10,15 @@ function OSClient() {
 }
 
 OSClient.prototype.getNFTs = async function(account, chain, collection = null) {
-    let r = await axios.post("https://api.opensea.io/graphql/",q_list_address)
+    let r = await axios.post("https://api.opensea.io/graphql/", this.q_list_address)
     console.log(r.data.data.query.search.edges)
+    let result = {
+        nfts: [],
+        collections: {}
+    }
     for (const iter_nft of r.data.data.query.search.edges) {
         let os_nft = iter_nft.node.asset
+        console.log(os_nft)
         let nft = {
             nft_contract: os_nft.assetContract.account.address.toLowerCase(),
             nft_id: os_nft.tokenId,
@@ -21,7 +26,20 @@ OSClient.prototype.getNFTs = async function(account, chain, collection = null) {
             nft_original_image: os_nft.imageUrl,
             nft_title: os_nft.name,
             nft_description: os_nft.description,
-            nft_owned: parseInt(os_nft.ownedQuantity)
+            nft_owned: parseInt(os_nft.ownedQuantity),
+            nft_chain: os_nft.assetContract.account.chain.identifier.toLowerCase()
+        }
+        let collection = {
+            contract_address: os_nft.assetContract.account.address.toLowerCase(),
+	image_url: os_nft.collection.imageUrl,
+	collection_name: os_nft.collection.name,
+	collection_description: os_nft.collection.description,
+	/*external_url varchar null,
+	collection_type NUMERIC null,
+	CONSTRAINT unique_collection UNIQUE (contract_address) */
+        }
+        if (collection == null || collection.toLowerCase() == nft.nft_contract) {
+            result.nfts.push(nft);
         }
       //  console.log(os_nft)
         console.log(nft)
