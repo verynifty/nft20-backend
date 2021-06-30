d3f798fdbffa3f7e7719055fcd7fde34ee7ab9b9
grandsmarquis <jdourlens@gmail.com>
2021-06-30 14:13:45 +0200

    Add univ3 spec to pool

--- etl/sql/nft20_pair.sql
+++ etl/sql/nft20_pair.sql
@@ -1,3 +1,7 @@
+-- Drop table
+
+-- DROP TABLE public.nft20_pair;
+
 CREATE TABLE public.nft20_pair (
 	address varchar NOT NULL,
 	nft varchar NULL,
@@ -10,7 +14,17 @@ CREATE TABLE public.nft20_pair (
 	nft_eth_price numeric NULL,
 	hidden bool NOT NULL DEFAULT false,
 	logo_url varchar NOT NULL DEFAULT 'https://space-cdn-dokomaps.fra1.digitaloceanspaces.com/nft20/placeholder.png'::character varying,
+	nft_value numeric NULL,
 	network numeric NOT NULL DEFAULT 0,
+	buy_price_eth numeric NULL,
+	sell_price_eth numeric NULL,
+	lp_version numeric NULL,
+	lp_fee numeric NULL,
 	CONSTRAINT nft20_pair_pk PRIMARY KEY (address),
 	CONSTRAINT unique_pair UNIQUE (address)
 );
+
+-- Permissions
+
+ALTER TABLE public.nft20_pair OWNER TO doadmin;
+GRANT ALL ON TABLE public.nft20_pair TO doadmin;
