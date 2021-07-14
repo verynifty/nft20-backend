b11859ce87733009d05b465648ba00e26cc27fad
grandsmarquis <jdourlens@gmail.com>
2021-07-14 18:05:27 +0200

    Pagination test

--- etl/utils/os_client.js
+++ etl/utils/os_client.js
@@ -14,58 +14,71 @@ function OSClient(storage) {
 OSClient.prototype.getNFTs = async function (account, chain, collection_filter = null) {
     this.q_list_address.variables.identity.address = account;
     this.q_list_address.variables.assetOwner.address = account;
-    let r = await axios.post("https://api.opensea.io/graphql/", this.q_list_address)
+    this.q_list_address.variables.cursor = null;
     let result = {
         nfts: [],
         collections: {}
     }
-    for (const iter_nft of r.data.data.query.search.edges) {
-        let os_nft = iter_nft.node.asset
-        if (os_nft == null) {
-            continue;
+    let run_query = true;
+    while (run_query) {
+
+        let r = await axios.post("https://api.opensea.io/graphql/", this.q_list_address)
+
+        for (const iter_nft of r.data.data.query.search.edges) {
+            let os_nft = iter_nft.node.asset
+            if (os_nft == null) {
+                continue;
+            }
+            console.log(os_nft)
+            let nft = {
+                nft_contract: os_nft.assetContract.account.address.toLowerCase(),
+                nft_id: os_nft.tokenId,
+                nft_image: os_nft.imageUrl,
+                nft_original_image: os_nft.imageUrl,
+                nft_title: os_nft.name,
+                nft_description: os_nft.description,
+            }
+            if (this.storage != null) {
+                await this.storage
+                    .insert("nft20_nft", nft)
+            }
+            nft.nft_owned = parseInt(os_nft.ownedQuantity),
+                nft.nft_chain = os_nft.assetContract.account.chain.identifier.toLowerCase()
+            let collecType = 721
+            if (os_nft.assetContract.tokenStandard == "ERC1155") {
+                collecType = 1155
+            }
+            let collection = {
+                contract_address: os_nft.assetContract.account.address.toLowerCase(),
+                image_url: os_nft.collection.imageUrl,
+                collection_name: os_nft.collection.name,
+                collection_description: os_nft.collection.description,
+                collection_type: collecType
+            }
+            if (this.storage != null) {
+                await this.storage
+                    .knex("nft20_collection")
+                    .insert(collection)
+                    .onConflict("contract_address")
+                    .merge();
+            }
+            if (nft.nft_chain == chain && (collection_filter == null || (collection_filter.toLowerCase() == nft.nft_contract))) {
+                result.nfts.push(nft);
+                result.collections[collection.contract_address] = collection;
+            }
+
+            if (r.data.data.query.search.pageInfo.hasNextPage && r.data.data.query.search.pageInfo.endCursor) {
+                this.q_list_address.variables.cursor = r.data.data.query.search.pageInfo.endCursor
+            } else {
+                run_query = false;
+            }
         }
-        console.log(os_nft)
-        let nft = {
-            nft_contract: os_nft.assetContract.account.address.toLowerCase(),
-            nft_id: os_nft.tokenId,
-            nft_image: os_nft.imageUrl,
-            nft_original_image: os_nft.imageUrl,
-            nft_title: os_nft.name,
-            nft_description: os_nft.description,
-        }
-        if (this.storage != null) {
-            await this.storage
-                .insert("nft20_nft", nft)
-        }
-        nft.nft_owned = parseInt(os_nft.ownedQuantity),
-        nft.nft_chain = os_nft.assetContract.account.chain.identifier.toLowerCase()
-        let collecType = 721
-        if (os_nft.assetContract.tokenStandard == "ERC1155") {
-            collecType = 1155
-        }
-        let collection = {
-            contract_address: os_nft.assetContract.account.address.toLowerCase(),
-            image_url: os_nft.collection.imageUrl,
-            collection_name: os_nft.collection.name,
-            collection_description: os_nft.collection.description,
-            collection_type: collecType
-        }
-        if (this.storage != null) {
-            await this.storage
-                .knex("nft20_collection")
-                .insert(collection)
-                .onConflict("contract_address")
-                .merge();
-        }
-        if (nft.nft_chain == chain && (collection_filter == null || (collection_filter.toLowerCase() == nft.nft_contract))) {
-            result.nfts.push(nft);
-            result.collections[collection.contract_address] = collection;
-        }
+
     }
     return (result)
 }
 
-OSClient.prototype.getNFT = async function(nft_contract, nft_id) {
+OSClient.prototype.getNFT = async function (nft_contract, nft_id) {
     this.q_single_nft.variables.archetype.assetContractAddress = nft_contract;
     this.q_single_nft.variables.archetype.tokenId = nft_id;
     let r = await axios.post("https://api.opensea.io/graphql/", this.q_single_nft)
@@ -86,13 +99,13 @@ OSClient.prototype.getNFT = async function(nft_contract, nft_id) {
     }
     console.log(nft)
     await this.storage
-    .knex("nft20_nft")
-    .insert(nft)
-    .onConflict(["nft_contract", "nft_id"])
-    .merge();
+        .knex("nft20_nft")
+        .insert(nft)
+        .onConflict(["nft_contract", "nft_id"])
+        .merge();
 }
 
-OSClient.prototype.getStats = async function() {
+OSClient.prototype.getStats = async function () {
     let r = await axios.post("https://api.opensea.io/graphql/", this.q_all_collections)
     if (r.data.data == null) {
         return;
@@ -105,7 +118,7 @@ OSClient.prototype.getStats = async function() {
         }
         //console.log(collection.node.assetContracts.edges)
     }
-    return(all_collections);
+    return (all_collections);
 }
 
 
