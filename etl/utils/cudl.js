0afad41ff6f9fd145815dbc6f51affcad3504c3a
grandsmarquis <jdourlens@gmail.com>
2021-07-15 17:12:23 +0200

    events for Cudl

---
+++ etl/utils/cudl.js
@@ -0,0 +1,154 @@
+const axios = require("axios");
+const BigNumber = require("bignumber.js");
+
+const sleep = (waitTimeInMs) =>
+    new Promise((resolve) => setTimeout(resolve, waitTimeInMs));
+
+function Cudl(ethereum, storage) {
+    this.ethereum = ethereum;
+    this.storage = storage;
+    this.ERC20ABI = require("../../contracts/ERC20.abi");
+    this.PETABI = require("../../contracts/pet.abi"); //TODO set ABI
+    this.pet = new ethereum.w3.eth.Contract( //TODO set address
+        this.PETABI,
+        ""
+    );
+}
+
+
+/* 
+
+event Mined(uint256 nftId, uint256 reward, address recipient);
+    event BuyAccessory(uint256 nftId, uint256 itemId, uint256 amount, uint256 itemTimeExtension, address buyer);
+    event Fatalize(uint256 opponentId, uint256 nftId, address killer);
+    event NewPlayer(
+        address nftAddress,
+        uint256 nftId,
+        uint256 playerId,
+        address owner
+    );
+*/
+
+Cudl.prototype.run = async function () {
+    let maxBlock = await this.ethereum.getLatestBlock();
+    let minBlock = await this.storage.getMax("pet", "blocknumber");
+    let events = null;
+    events = await this.game.getPastEvents("Mined", {
+        fromBlock: minBlock,
+        toBlock: maxBlock,
+    });
+    for (const event of events) {
+        let tx = await this.ethereum.getTransaction(event.transactionHash);
+        let timestamp = await this.ethereum.getBlockTimestamp(event.blockNumber);
+        await this.storage.insert("game_claim", {
+            blocknumber: event.blockNumber,
+            transactionhash: this.ethereum.normalizeHash(event.transactionHash),
+            from: this.ethereum.normalizeHash(tx.from),
+            to: this.ethereum.normalizeHash(tx.to),
+            logindex: event.logIndex,
+            timestamp: new Date(parseInt(timestamp * 1000)).toUTCString(),
+            gasprice: tx.gasPrice,
+            pet: event.returnValues.nftId,
+            amount: event.returnValues.reward,
+            recipient: this.ethereum.normalizeHash(event.returnValues.recipient)
+        });
+        await this.updatePet(event.returnValues.nftId)
+    }
+    events = await this.game.getPastEvents("BuyAccessory", {
+        fromBlock: minBlock,
+        toBlock: maxBlock,
+    });
+    for (const event of events) {
+        let tx = await this.ethereum.getTransaction(event.transactionHash);
+        let timestamp = await this.ethereum.getBlockTimestamp(event.blockNumber);
+        await this.storage.insert("game_claim", {
+            blocknumber: event.blockNumber,
+            transactionhash: this.ethereum.normalizeHash(event.transactionHash),
+            from: this.ethereum.normalizeHash(tx.from),
+            to: this.ethereum.normalizeHash(tx.to),
+            logindex: event.logIndex,
+            timestamp: new Date(parseInt(timestamp * 1000)).toUTCString(),
+            gasprice: tx.gasPrice,
+            pet: event.returnValues.nftId,
+            item: event.returnValues.itemId,
+            amount_paid: event.returnValues.amount,
+            time_extension: event.returnValues.itemTimeExtension,
+            buyer: this.ethereum.normalizeHash(event.returnValues.buyer)
+        });
+        await this.updatePet(event.returnValues.nftId)
+    }
+    events = await this.game.getPastEvents("Fatalize", {
+        fromBlock: minBlock,
+        toBlock: maxBlock,
+    });
+    for (const event of events) {
+        let tx = await this.ethereum.getTransaction(event.transactionHash);
+        let timestamp = await this.ethereum.getBlockTimestamp(event.blockNumber);
+        await this.storage.insert("game_claim", {
+            blocknumber: event.blockNumber,
+            transactionhash: this.ethereum.normalizeHash(event.transactionHash),
+            from: this.ethereum.normalizeHash(tx.from),
+            to: this.ethereum.normalizeHash(tx.to),
+            logindex: event.logIndex,
+            timestamp: new Date(parseInt(timestamp * 1000)).toUTCString(),
+            gasprice: tx.gasPrice,
+            victim: event.returnValues.victim,
+            winner:  event.returnValues.winner,
+            badguy: this.ethereum.normalizeHash(event.returnValues.killer)
+        });
+        await this.updatePet(event.returnValues.victim)
+        await this.updatePet(event.returnValues.winner)
+    }
+    events = await this.game.getPastEvents("NewPlayer", {
+        fromBlock: minBlock,
+        toBlock: maxBlock,
+    });
+    for (const event of events) {
+        let tx = await this.ethereum.getTransaction(event.transactionHash);
+        let timestamp = await this.ethereum.getBlockTimestamp(event.blockNumber);
+        await this.storage.insert("game_claim", {
+            blocknumber: event.blockNumber,
+            transactionhash: this.ethereum.normalizeHash(event.transactionHash),
+            from: this.ethereum.normalizeHash(tx.from),
+            to: this.ethereum.normalizeHash(tx.to),
+            logindex: event.logIndex,
+            timestamp: new Date(parseInt(timestamp * 1000)).toUTCString(),
+            gasprice: tx.gasPrice,
+            originnft: this.ethereum.normalizeHash(event.returnValues.nftAddress),
+            originid: this.ethereum.normalizeHash(event.returnValues.nftId),
+            victim: event.returnValues.playerId,
+            owner: this.ethereum.normalizeHash(event.returnValues.owner),
+        });
+        await this.updatePet(event.returnValues.playerId)
+    }
+}
+
+Cudl.prototype.updatePet = async function (playerId) {
+    if (id == null) {
+        return;
+    }
+    try {
+        let infos = await this.pet.methods.getInfo(playerId).call();
+        let player = {
+            player_id: infos._playerId,
+            is_alive: infos._isAlive,
+            score: infos._score,
+            expected_reward: infos._expectedReward,
+            time_until_death: infos._timeUntilDeath,
+            time_born: setDead == false ? new Date(parseInt(infos._timeBorn * 1000)).toUTCString() : null,
+            owner: this.ethereum.normalizeHash(infos._owner),
+            nft_contract: this.ethereum.normalizeHash(infos._nftOrigin),
+            nft_id: infos._nftId,
+            tod: new Date(parseInt(infos._timeOfDeath * 1000)).toUTCString()
+        }
+        await this.storage
+            .knex("cudl_pet")
+            .insert(player)
+            .onConflict("player_id")
+            .merge();
+    } catch (error) {
+        console.log("Failed to update pet cause ", error)
+    }
+}
+
+module.exports = Cudl;
