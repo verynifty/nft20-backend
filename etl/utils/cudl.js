6a3aeb04bbea59d1763b1f9c4ed2c62e50756210
grandsmarquis <jdourlens@gmail.com>
2021-07-18 13:24:35 +0200

    reorg protection

--- etl/utils/cudl.js
+++ etl/utils/cudl.js
@@ -42,7 +42,7 @@ function Cudl(ethereum, storage) {
 */
 
 Cudl.prototype.run = async function () {
-    let maxBlock = await this.ethereum.getLatestBlock();
+    let maxBlock = await this.ethereum.getLatestBlock() - 2;
     let deployed_block = 12847722;
     let minBlock = Math.min(deployed_block, await this.storage.getMax("cudl_mined", "blocknumber"),  await this.storage.getMax("cudl_feed", "blocknumber"));
     let events = null;
