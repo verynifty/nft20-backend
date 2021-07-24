e4dc31b962d7cbc4320c217754f8c5c12dc8296e
grandsmarquis <jdourlens@gmail.com>
2021-07-24 17:13:33 +0200

    dumb dave

--- api/index.js
+++ api/index.js
@@ -660,7 +660,7 @@ app.post("/pepeswantstovote", async function (req, res) {
 });
 
 app.get("/price/hour", async function(req, res) {
-  let data = this.storage.knex.select("*").from("nft20_price_feed_hour_view").where("nft_address", req.query.address.toLowerCase())
+  let data = await this.storage.knex.select("*").from("nft20_price_feed_hour_view").where("nft_address", req.query.address.toLowerCase())
   res.status(200).json(data);
 }) 
 
